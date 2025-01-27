7f9f51dad0acba350848164acaa63bb333dc0fa8
Mauricio Cardoso <mauricio.cds00@gmail.com>
2025-01-27 16:17:29 -0300

    feat: add cache

--- pkg/renders/render.go
+++ pkg/renders/render.go
@@ -1,21 +1,71 @@
 package renders
 
 import (
-	"fmt"
+	"bytes"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedT, err := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
-
+	tc, err := createTemplate()
 	if err != nil {
-		fmt.Println("Error" + err.Error())
+		log.Fatal(err)
 	}
 
-	err = parsedT.Execute(w, nil)
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(err)
+	}
+
+	buf := new(bytes.Buffer)
+
+	err = t.Execute(buf, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createTemplate() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
 
 	if err != nil {
-		fmt.Println("Error" + err.Error())
+		return myCache, err
 	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).ParseFiles(page)
+
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
 }
